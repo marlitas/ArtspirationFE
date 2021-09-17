ed135674ed6cf86eef3396cd61f29552fe8307bf
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-16 20:29:27 -0700

    Create index specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :dashboard, only: :index
+  resources :dashboard, only: :index do
+    resources :artwork, only: [:index]
+  end
 end
---
+++ spec/features/artworks/index_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require './spec/services/web_mock_stubs'
+
+RSpec.describe 'index' do
+  describe 'user' do
+    before(:each) do
+      user_stub = WebmockStubs.mock_user
+      stub_request(:get, 'https://www.localhost:3000/api/v1/users/1')
+      .to_return(status: 200, body: user_stub, headers: {})
+      art_stub = WebmocksStubs.mock_artworks
+      stub_request(:get, 'https://www.localhost:3000/api/v1/users/1/rated_art')
+      .to_return(status: 200, body: art_stub, headers: {})
+
+      @user = UsersFacade.get_user(1)
+      @artworks = ArtworksFacade.get_rated_art(@user.id)
+    end
+
+    it 'sees all art they have liked' do
+      visit '/dashboard/artworks'
+
+      expect(page).to have_content("#{user.name}'s Favorite Artwork")
+      expect(page).to have_selector('art', count: 3)
+    end
+
+    it 'sees the titles, artists, and jpeg render for each artwork' do
+      visit '/dashboard/artworks'
+
+      within("##{@artworks[0].id}") do
+        expect(page).to have_css("img[src*=#{@artworks[0].url}]")
+        expect(page).to have_content(@artworks[0].title)
+        expect(page).to have_content(@artworks[0].artist)
+      end
+
+      within("##{@artworks[1].id}") do
+        expect(page).to have_css("img[src*=#{@artworks[1].url}]")
+        expect(page).to have_content(@artworks[1].title)
+        expect(page).to have_content(@artworks[1].artist)
+      end
+
+      within("##{@artworks[2].id}") do
+        expect(page).to have_css("img[src*=#{@artworks[2].url}]")
+        expect(page).to have_content(@artworks[2].title)
+        expect(page).to have_content(@artworks[2].artist)
+      end
+
+      it 'sees alt text if image does not render' do
+        visit '/dashboard/artworks'
+      end
+    end
+
+    describe 'links' do
+      it 'can link to artwork show page' do
+
+      end
+    end
+  end
+end
