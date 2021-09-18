e48038c5240815a9d238bbe6c5eeda4cfbc77794
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-18 04:06:55 -0700

    Add liked art variable into index method for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @user = UsersFacade.get_user(session[:user_id])
-    require "pry";binding.pry
+    @liked_art = ArtworksFacade.get_rated_art(session[:user_id])
   end
 end
