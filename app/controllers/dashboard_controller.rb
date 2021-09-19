122f57c3125051f05b482299b50e1d7206812d5b
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-19 15:43:32 -0700

    Add recommendation to dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @user = UsersFacade.get_user(session[:user_id])
     @liked_art = ArtworksFacade.get_rated_art(session[:user_id])
+    @recommended_art = ArtworksFacade.get_artwork_recommendations(session[:user_id])
   end
 end
