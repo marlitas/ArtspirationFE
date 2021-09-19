840c264cb8f892679e530bff6098e7b320f9ede4
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-19 16:50:21 -0700

    Add authorization to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :user_login
+
   def current_user
     UsersFacade.get_user(session[:user_id]) if session[:user_id]
   end
+
+  def user_login
+    if !current_user
+      redirect_to root_path
+      flash[:alert] = 'You must be logged in to visit that page.'
+    end
+  end
+
 end
