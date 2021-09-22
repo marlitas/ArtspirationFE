f7780a00c40512d42073fb7fdb87ead9472f135d
deebot10 <dee1jhill@gmail.com>
2021-09-22 15:21:06 -0600

    Fix singular url

--- app/services/artworks_service.rb
+++ app/services/artworks_service.rb
@@ -15,12 +15,12 @@ class ArtworksService
   end
 
   def self.get_art(art_id, user_id)
-    response = ArtworksService.connect.get("/api/v1/users/#{user_id}/rated_art/#{art_id}")
+    response = ArtworksService.connect.get("/api/v1/users/#{user_id}/rated_arts/#{art_id}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.update_art(art_id, user_id, content)
-    response = ArtworksService.connect.patch("/api/v1/users/#{user_id}/rated_art/#{art_id}") do |req|
+    response = ArtworksService.connect.patch("/api/v1/users/#{user_id}/rated_arts/#{art_id}") do |req|
       req.body = content.to_json
     end
     JSON.parse(response.body, symbolize_names: true)
