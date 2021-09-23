53de395188d0f23dc8bd74e909a8971f4d9b349b
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-23 16:38:02 -0700

    Update nav bar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class DashboardController < ApplicationController
   def index
-
     @user = current_user
     @liked_art = ArtworksFacade.get_rated_art(@user.id)
     @recommended_art = ArtworksFacade.get_artwork_recommendations(@user.id)
